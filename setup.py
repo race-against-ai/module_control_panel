a797d7bc91dbb3b6b82fd2af485bdb8138655241
Calvin Teuber <112952501+rexari03@users.noreply.github.com>
2023-07-04 14:15:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,15 +9,15 @@ def read(fname):
 
 
 setup(
-    name="raai_module_template",
+    name="raai_module_control_panel",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    author="NGITl",
-    author_email="your@email.de",
-    description=("RAAI Module Template for managing different projects for RAAI"),
+    author="Calvin Teuber",
+    author_email="calvinteuber7@gmail.com",
+    description=("Control panel module which contains the entire control UI of RaceAgainstAI"),
     license="GPL 3.0",
-    keywords="template",
-    url="https://github.com/vw-wob-it-edu-ngitl/raai_module_template",
+    keywords="module",
+    url="https://github.com/vw-wob-it-edu-ngitl/raai_module_control_panel",
     packages=find_packages(),
     long_description=read("README.md"),
     install_requires=["pynng~=0.7.2"],
